1816cb9fedbfb887fa7d516e848696cafa9ab6ca
dongchanhong <beat_chani@naver.com>
2020-09-07 23:39:06 +0900

    fist

---
+++ js/function.js
@@ -0,0 +1,77 @@
+var inputtableList = '';
+var tableList = 10;
+var myArrayNum = myArray;
+function getInputValue() {
+    var inputtableList = document.getElementById('pageSet');
+    alert(inputtableList.value);
+    tableList = parseInt(inputtableList.value);
+    buildTable(myArrayNum,page=1)
+}
+buildTable(myArrayNum,page=1)
+
+
+
+
+console.log(tableList);
+//화면에 보여질 tableList수
+
+
+buildTable(myArrayNum, page=1) 
+function buildTable(data, page) { 
+    var table = document.getElementById('table1') 
+    var totalPage = Math.ceil(data.length / tableList);
+    // 소수점 첫째자리 올림
+    
+    var startPage = ((page-1)*tableList)+1;
+    var endPage = startPage+tableList-1;
+
+    var paginationHTML = '';
+    table.innerHTML = '';
+ 
+    var tableHead = `<tr> 
+                      <th>상품</th> 
+                      <th>가격</th> 
+                      <th>이미지</th> 
+                      <th>바로가기</th>
+                    </tr>` 
+
+    table.innerHTML = tableHead
+    for (var i=startPage; i < endPage+1; i++) { 
+        var row = `<tr> 
+                    <td>${data[i].productName}</td> 
+                    <td style="width:150px">${data[i].productPrice}원</td> 
+                    <td style="width:100px; height:100px;"><img src=${data[i].productImage} width="100" height="100"></td> 
+                    <td><a href=${data[i].productUrl} target="_blank">최저가</a></td> 
+                  </tr>` 
+        table.innerHTML += row 
+    } 
+   
+    
+
+    for (var i=1; i < totalPage+1; i++ ) {
+        var pagingID = "ID"+i;
+        // className을 개별 지정해둬야 클릭 시 이벤트를 할당할 수 있음
+        paginationHTML += "<a style='cursor:pointer'" + "class=" + pagingID + ">" + "  "  +i+ "  </a>";
+        console.log(pagingID);
+        }
+    
+    pagination.innerHTML = paginationHTML;
+    console.log(startPage);
+    console.log(endPage);
+
+
+
+    for (var i=1; i < totalPage+1; i++) {
+    if (!!document.querySelector(".ID"+i)) {pageClick(i);}
+    }
+
+    //if (!!document.querySelector(".ID"+i)) {pageClick(i);}
+    //if (!!document.querySelector(".ID2"+2)) {pageClick(2);}
+}
+
+function pageClick(pageNum) {
+    document.querySelector(".ID"+pageNum).addEventListener('click', () => {
+            buildTable(myArrayNum,pageNum);
+            console.log("ID"+pageNum+" 생성");
+        });
+}
---
+++ js/loader.js
@@ -0,0 +1,5 @@
+function loader() {
+    document.getElementById("loader").style.display = "block";
+}
+
+
